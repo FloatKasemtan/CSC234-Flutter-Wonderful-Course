271a18cbfdff8a77744f277bed1ac7da92f40c95
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2022-02-09 01:01:06 +0700

    feat: add darkmode

--- layout_interactive_excercise/lib/main.dart
+++ layout_interactive_excercise/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:layout_interactive_excercise/widgets/ParentWidget.dart';
 
 void main() {
@@ -8,22 +11,79 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.light);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: const TextTheme(
-            headline1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            bodyText1: TextStyle(
-                fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.9),
-            subtitle1: TextStyle(color: Colors.black38, fontSize: 14),
-            bodyText2:
-                TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
-        primarySwatch: Colors.blue,
+    const TextTheme textTheme = TextTheme(
+      headline1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.9),
+      subtitle1: TextStyle(color: Colors.black38, fontSize: 14),
+      bodyText2: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+    );
+
+    const TextTheme textThemeDark = TextTheme(
+      headline1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.9),
+      subtitle1: TextStyle(color: Colors.white38, fontSize: 14),
+      bodyText2: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+    );
+
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: themeNotifier,
+        builder: (_, ThemeMode currentMode, __) {
+          return MaterialApp(
+            // Remove the debug banner
+            debugShowCheckedModeBanner: false,
+            title: '63130500237 Flutter app',
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.blue,
+              textTheme: textTheme,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primaryColor: Colors.amber,
+              textTheme: textThemeDark,
+            ),
+            themeMode: currentMode,
+            home: const Home(),
+          );
+        });
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isDarkMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const ParentWidget(),
+      appBar: AppBar(
+        title: const Text("Layout interactive"),
+        actions: [
+          IconButton(
+              icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+              onPressed: () {
+                setState(() {
+                  isDarkMode = !isDarkMode;
+                });
+                MyApp.themeNotifier.value =
+                    MyApp.themeNotifier.value == ThemeMode.light
+                        ? ThemeMode.dark
+                        : ThemeMode.light;
+              })
+        ],
       ),
-      home: const Scaffold(body: ParentWidget()),
     );
   }
 }
